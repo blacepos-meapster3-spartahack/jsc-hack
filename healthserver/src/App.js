a35d24c67b05b00cadb810b7e52daff70139631f
cmulholland <20cmulholland@gmail.com>
2024-11-16 14:29:11 -0600

    added chartjs

--- healthserver/src/App.js
+++ healthserver/src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+} from 'chart.js';
+import { Line, Bar } from 'react-chartjs-2';
 
-function App() {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+
+export const data = {
+  labels,
+  datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1,
+    borderColor: 'rgba(75,192,192,1)',
+    backgroundColor: 'rgba(75,192,192,0.2)',
+  }],
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div>
-        <canvas id="myChart"></canvas>
-      </div>
-
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-      <script>
-        const ctx = document.getElementById('myChart');
-      </script>
+    <div>
+      <Bar options={options} data={data} />
+      <Bar options={options} data={data} />
     </div>
   );
 }
-
-export default App;
