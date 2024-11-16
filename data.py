dadc06095a7186f62ee6921a3f8ff174e97a8915
Blacepos <jraustin999@gmail.com>
2024-11-16 11:17:20 -0600

    a

--- data.py
+++ data.py
@@ -2,18 +2,19 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import List
 
+# total 360+4+15+26
 @dataclass
 class Metrics:
-    # Heartrate
+    # Heartrate measurements (360)
     heartrate_bpm: List[float]
 
-    # Sleep
+    # Sleep (total 4)
     previous_night_awake_minutes: float
     previous_night_rem_minutes: float
     previous_night_light_minutes: float
     previous_night_deep_minutes: float
 
-    # Food
+    # Food (total 5 * 3)
     meal_1_breakfast: float
     meal_2_breakfast: float
     meal_3_breakfast: float
@@ -32,7 +33,7 @@ class Metrics:
     meal_4_dinner: float
     meal_5_dinner: float
 
-    # Subjective measurements
+    # Subjective measurements (total 13 * 2)
     frustration_morning: float
     stomach_ache_morning: float
     anxiety_morning: float
@@ -45,9 +46,9 @@ class Metrics:
     vomiting_morning: float
     stuffy_nose_morning: float
     sore_throat_morning: float
-    concentration_evening: float
+    concentration_morning: float
 
-    frustration_morning: float
+    frustration_evening: float
     stomach_ache_evening: float
     anxiety_evening: float
     headache_evening: float
@@ -60,3 +61,10 @@ class Metrics:
     stuffy_nose_evening: float
     sore_throat_evening: float
     concentration_evening: float
+
+    def default() -> Metrics:
+        return Metrics(
+            [0 for _ in range(360)],
+            *[0 for _ in range(4+15+26)]
+        )
+    
--- generate.py
+++ generate.py
@@ -1,11 +1,14 @@
 """
 
 """
-
 from typing import List
-
 from data import Metrics
+
+def person_1_predict():
+    # 
+    heart_correlation_person_2 = 0.8
+
 
 def gen_from_history(history: List[Metrics]) -> Metrics:
     # for the past 10 measurements, determine the trend
-    pass
+    metrics = Metrics.default()
