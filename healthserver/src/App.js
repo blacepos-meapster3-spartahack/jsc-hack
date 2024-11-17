22309b13355ab23ca7a507602b8cf837fc4c02c7
cmulholland <20cmulholland@gmail.com>
2024-11-16 19:06:57 -0600

    everything init

--- healthserver/src/App.js
+++ healthserver/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/header.js';
 import Heartbeat from './components/Heartbeat.js';
+import Sleep from './components/Sleep.js';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -26,25 +27,6 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  aspectRatio: 1.5, // Adjust this value to change the aspect ratio
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Heatbeats',
-    },
-  },
-  scales: {
-    y: {
-      beginAtZero: true
-    },
-  }
-};
-
 const labels = ['5', '4', '3', '2', '1', '0'];
 
 export const data = {
@@ -59,36 +41,55 @@ export const data = {
 };
 
 export default function App() {
-  const [text, setText] = useState('');
+  const [formParams, setFormParams] = useState({
+    astronaut: '',
+  });
+
+  const handleButtonClick = (astronaut) => {
+    setFormParams({ astronaut });
+  }
 
   return (
     <div>
       <Header />
       <div className="container-fluid" style={{ padding: "5px" }}>
-            <div className="row" style={{ minHeight: '200px' }}>
-                <div className="col-md-3 chart-container">
-                    <Heartbeat options={options} data={data} />
-                </div>
-                <div className="col-md-3 chart-container">
-                    <Heartbeat options={options} data={data} />
-                </div>
-                <div className="col-md-3 chart-container">
-                    <Heartbeat options={options} data={data} />
-                </div>
-                <div className="col-md-3 chart-container">
-                    <Heartbeat options={options} data={data} />
-                </div>
-            </div>
+        <div className="row" style={{ minHeight: '200px' }}>
+          <div className="col-md-3 chart-container">
+            <Heartbeat data={data} text={"Heartbeat of Astronaut 1"} />
+          </div>
+          <div className="col-md-3 chart-container">
+            <Heartbeat data={data} text={"Heartbeat of Astronaut 2"} />
+          </div>
+          <div className="col-md-3 chart-container">
+            <Heartbeat data={data} text={"Heartbeat of Astronaut 3"} />
+          </div>
+          <div className="col-md-3 chart-container">
+            <Heartbeat data={data} text={"Heartbeat of Astronaut 4"} />
+          </div>
         </div>
+      </div>
       <hr style={{ border: '2px solid black', margin: '20px 0' }} />
-      <div className="button-container" style={{ textAlign: 'left', margin: '20px 0' }}>
-        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => setText('Astronaut 1 Selected')}>1</button>
-        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => setText('Astronaut 2 Selected')}>2</button>
-        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => setText('Astronaut 3 Selected')}>3</button>
-        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => setText('Astronaut 4 Selected')}>4</button>
+      <div className="button-container" style={{ textAlign: 'center', margin: '20px 0' }}>
+        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => handleButtonClick(1)}>1</button>
+        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => handleButtonClick(2)}>2</button>
+        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => handleButtonClick(3)}>3</button>
+        <button className="btn btn-outline-dark" style={{ margin: '0 0.1%' }} onClick={() => handleButtonClick(4)}>4</button>
       </div>
       <div className="text-box" style={{ textAlign: 'center', margin: '20px 0' }}>
-        <input type="text" value={text} readOnly className="form-control" />
+        <input type="text" value={"Astronaut " + formParams.astronaut + ":"} readOnly className="form-control text-center" />
+      </div>
+      <div className="container-fluid" style={{ padding: "5px" }}>
+        <div className="row justify-content-center" style={{ minHeight: '200px' }}>
+          <div className="col-md-3 chart-container">
+            <Sleep data={data} text={"Amount of light sleep"} />
+          </div>
+          <div className="col-md-3 chart-container">
+            <Sleep data={data} text={"Amount of heavy sleep"} />
+          </div>
+          <div className="col-md-3 chart-container">
+            <Sleep data={data} text={"Amount of REM sleep"} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- healthserver/src/components/Heartbeat.js
+++ healthserver/src/components/Heartbeat.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
-const Heartbeat = ({ options, data }) => {
+const Heartbeat = ({ data, text }) => {
     return (
-        <Line options={options} data={data} />
+        <Line options={
+        {
+            responsive: true,
+            aspectRatio: 1.5, // Adjust this value to change the aspect ratio
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: text,
+                },
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+            }
+        }} data={data} />
     );
 };
 
